de66de9d8df33ac42273101362aa7e9c89c67b01
SFYLL <santiagoflood@hotmail.fr>
2023-05-02 15:36:18 +0200

    new test case

--- test/test_script/involution.test.ts
+++ test/test_script/involution.test.ts
@@ -11,27 +11,32 @@ import {
 
   import { readFileSync } from 'fs';
   
+  function testSetup(fileExtension: string, desiredRowsLength: number, desiredColumnsLength: number) {
+    const baseDirectory = getRootProjectDirectory();
+
+    const imageData = generateImageData(
+        fileExtension,
+        desiredRowsLength,
+        desiredColumnsLength
+    );
+
+    const matrixInputs: string[] = imageData.convertToProofTest();
+
+    const witness = {
+        rows: imageData.rows,
+        columns: imageData.pixelsPerRow,
+        image: matrixInputs,
+    };
+    return { witness, baseDirectory, imageData };
+}
+  
   describe('Involution Circuit Tests', () => {
     async function runTest(
       fileExtension: string,
       desiredRowsLength: number = 200,
       desiredColumnsLength: number = 200,
     ) {
-      const baseDirectory = getRootProjectDirectory();
-  
-      const imageData = generateImageData(
-        fileExtension,
-        desiredRowsLength,
-        desiredColumnsLength,
-      );
-  
-      const matrixInputs: string[] = imageData.convertToProofTest();
-  
-      const witness = {
-        rows: imageData.rows,
-        columns: imageData.pixelsPerRow,
-        image: matrixInputs,
-      };
+      const { witness, baseDirectory, imageData } = testSetup(fileExtension, desiredRowsLength, desiredColumnsLength);
   
       try {
         const { proof, publicSignals } = await groth16.fullProve(
@@ -58,16 +63,16 @@ import {
       }
     }
   
-    test('Failure due to wrong inputs (rows > desiredRowsLength)', async () => {
-        const fileExtension = '/50_50_image_base_test.json';
-        const desiredRowsLength = 51;
-        //remove consol error as they are expected
-        const originalConsolError = consoleErrorDisable();
-        const { error, result } = await runTest(fileExtension, desiredRowsLength);
-        consoleErrorEnable(originalConsolError);
-        expect(result).toBe(false);
-        expect(error).toBe('Error: Too many signals set. ');
-      });
+    // test('Failure due to wrong inputs (rows > desiredRowsLength)', async () => {
+    //     const fileExtension = '/50_50_image_base_test.json';
+    //     const desiredRowsLength = 51;
+    //     //remove consol error as they are expected
+    //     const originalConsolError = consoleErrorDisable();
+    //     const { error, result } = await runTest(fileExtension, desiredRowsLength);
+    //     consoleErrorEnable(originalConsolError);
+    //     expect(result).toBe(false);
+    //     expect(error).toBe('Error: Too many signals set. ');
+    //   });
       
       test('Failure due to wrong inputs (columns > desiredColumnsLength)', async () => {
         const fileExtension = '/50_50_image_base_test.json';
@@ -84,11 +89,26 @@ import {
         expect(result).toBe(false);
         expect(error).toBe('Error: Too many signals set. ');
     });
-    // test('Failure when an element is not 0 in the right-padded part of the matrix', async () => {
-    //   const fileExtension = '/50_50_image_base_test.json'; 
-    //   const res = await runTest(fileExtension);
-    //   expect(res).toBe(false);
-    // });
+    test('Failure when an element is not 0 in the right-padded part of the matrix, as it is a violation of the given dimensions', async () => {
+      const fileExtension = '/small_image_metadata.json'; 
+      let { witness, baseDirectory, imageData } = await testSetup(fileExtension, 50, 200);
+
+      witness.image[imageData.desiredRowsLength * imageData.desiredColumnsLength - 1] = "157";
+
+      //remove consol error as they are expected
+      const originalConsolError = consoleErrorDisable();
+      try {
+        const { proof, publicSignals } = await groth16.fullProve(
+          witness,
+          baseDirectory + '/circuits/build/involution_js/involution.wasm',
+          baseDirectory + '/circuits/zkFiles/involution_final.zkey',
+        );
+        }
+        catch (error) {
+            consoleErrorEnable(originalConsolError);
+            expect((error as Error).message).toContain('Error in template VerifyImageBounds_5 line: 54');
+        }
+    });
   
     // test('Proof success when fetching another JSON', async () => {
     //   const fileExtension = '/50_50_image_base_test.json';
@@ -98,4 +118,3 @@ import {
     //   expect(res).toBe(true);
     // });
   });
-  
