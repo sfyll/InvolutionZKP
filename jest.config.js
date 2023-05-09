0d7b93a33b6dc75f27c670c0c51cb175d85d0378
SFYLL <santiagoflood@hotmail.fr>
2023-05-09 17:12:28 +0200

    packages

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 // jest.config.js
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    testMatch: ['**/test/test_script/**/*.test.ts'],
-  };
+  preset: "ts-jest",
+  testEnvironment: "node",
+  testMatch: ["**/test/test_script/**/*.test.ts"]
+};
